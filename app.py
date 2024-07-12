9c67539b0f6f686a856df858edc19ba4df0abfbb
AdityaPurswani <adityapurswani9621@gmail.com>
2024-07-12 23:48:09 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,85 @@
+from shiny.express import input, render, ui, session
+from shiny import reactive, req
+from shinymedia import input_video_clip, audio_spinner
+from model import chat
+from faicons import icon_svg
+from htmltools import css
+
+messages = []
+
+input_video_clip(
+    "clip",
+    reset_on_record=True,
+    class_="mt-3 mx-auto",
+    style=css(width="600px", max_width="100%"),
+    video_bits_per_second=256000,
+    audio_bits_per_second=64000,
+)
+
+@reactive.extended_task
+async def chat_task(video_clip, messages, session):
+    with ui.Progress(session=session) as p:
+        chat_output = await chat(video_clip, messages, p)
+        return chat_output, messages
+
+@reactive.effect
+@reactive.event(input.clip, ignore_none=False)
+def start_chat():
+    chat_task.cancel()
+    req(input.clip())
+    chat_task(input.clip(), messages[:], session)
+    
+@render.express
+def response():
+    
+    if chat_task.status() == "initial":
+        with ui.card(class_="mt-3 mx-auto", style=css(width="600px", max_width="100%")):
+            ui.markdown(
+                """
+                **Instructions:** Record a short video clip to start chatting with GPT-4o.
+                After it responds, you can record another clip to continue the conversation.
+                Reload the browser to start a new conversation.
+
+                Some ideas to get you started:
+
+                * “What do you think of the outfit I'm wearing?”
+                * “Where does it look like I am right now?”
+                * “Tell me an interesting fact about an object you see in this video.”
+                """
+            )
+        return
+    
+    if input.clip() is None or chat_task.status() == "running":
+        return
+    
+    chat_result_audio, chat_result_messages = chat_task.result()
+
+    global messages
+    messages = chat_result_messages[:]
+    
+    audio_spinner(src=chat_result_audio)
+    
+with ui.panel_fixed(bottom=0, left=0, right=0, height="auto", id="footer"):
+    with ui.div(class_="mx-auto", style=css(width="600px", max_width="100%")):
+        with ui.div(class_="float-left"):
+            "Built in Python with "
+            ui.a("Shiny", href="https://shiny.posit.co/py/")
+        with ui.div(class_="float-right"):
+            with ui.a(href="https://github.com/AdityaPurswani"):
+                icon_svg("github", margin_right="0.5em")
+                "View source code"
+                
+ui.head_content(
+    ui.tags.style(
+        """
+        #footer {
+            padding: 0.5em 0.7em;
+            background-color: var(--bs-primary);
+            color: white;
+        }
+        #footer a {
+            color: white;
+        }
+        """
+    )
+)
---
+++ model.py
@@ -0,0 +1,69 @@
+import os
+from pathlib import Path
+
+import dotenv
+from openai import AsyncOpenAI
+from shiny import ui
+from media_extractor import split_video
+import datauri
+
+dotenv.load_dotenv()
+if os.environ.get("OPENAI_API_KEY") is None:
+    raise ValueError("OPENAI_API_KEY not found in .env file")
+
+client = AsyncOpenAI()
+
+async def chat(video_file: str, messages: list[any], progress: ui.Progress) -> str:
+    progress.set(message="Decoding input...", value=0)
+    audio_uri, image_uris = split_video(video_file)
+
+    # Decode the audio file into text, using OpenAI's `whisper-1` model. The result will serve as the text prompt for the LLM.
+
+    progress.set(message="Transcribing audio...", value=0.1)
+    with datauri.as_tempfile(audio_uri) as audio_file:
+        transcription = await client.audio.transcriptions.create(
+            model="whisper-1", file=Path(audio_file)
+        )
+        
+    user_prompt = transcription.text
+    
+    progress.set(message="Waiting for response...", value=0.2)
+    
+    response = await client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                'role': 'user',
+                'content': [
+                    {'type':'text', 'text': user_prompt},
+                    *[
+                        {
+                            'type': 'image_url',
+                            'image_url': {'url': image_uri, 'detail':'auto'},
+                        }
+                        for image_uri in image_uris
+                    ]
+                ]
+            },
+            {
+                'role':'system',
+                'content': Path("system_prompt.txt").read_text()
+            },
+        ],
+    )
+    
+    response_text = response.choices[0].message.content
+    
+    # Maintaining History
+    messages.append(response.choices[0].message)
+    
+    
+    progress.set(message="Synthesizing audio...", value=0.8)
+    audio = await client.audio.speech.create(
+        model='tts-1',
+        voice='nova',
+        input=response_text,
+        response_format='mp3',
+    )
+    response_audio_uri = datauri.from_bytes(audio.read(), "audio/mpeg")
+    return response_audio_uri
